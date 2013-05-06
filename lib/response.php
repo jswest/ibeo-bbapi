4ffb22040c408792c2cc654fec24c3ce35fc35d2
John West <john.sheffield.west@gmail.com>
2013-05-06 09:25:28 -0400

    Modifies the lib/response file.

--- lib/response.php
+++ lib/response.php
@@ -10,27 +10,33 @@ class BackboneResponse {
     $this->exclude = $exclude;
     $this->set_response();
   }
+
+  function get_post_data( $post ) {
+    $result = array();
+    $cat = get_the_category( $post->ID );
+    $result['category'] = $cat[0]->name;
+    $result['category_id'] = $cat[0]->term_id;
+    $result['category_slug'] = $cat[0]->slug;
+    $result['tags'] = get_the_tags( $post->ID );
+    $result['post_id'] = $post->ID;
+    $result['slug'] = $post->post_name;
+    $result['content'] = apply_filters( 'the_content', $post->post_content );
+    $result['thumbnail'] = get_the_post_thumbnail( $post->ID, 'thumbnail' );
+    $result['image'] = get_the_post_thumbnail( $post->ID, 'full', array( 'class' => 'attachment' ) );
+    $result['title'] = $post->post_title;
+    $result['date'] = $post->post_date;
+    $name = get_userdata( $post->post_author );
+    $result['lastname'] = $name->user_lastname;
+    $result['firstname'] = $name->user_firstname;
+    $result['tags'] = wp_get_post_tags( $post->ID );
+    return $result;
+  }
   
   function set_response() {
     // Get a single post.
     if( $this->type == 'post' ) {
       $post = get_post( $this->id );
-      $result = array();
-      $cat = get_the_category( $post->ID );
-      $result['category'] = $cat[0]->name;
-      $result['category_id'] = $cat[0]->term_id;
-      $result['category_slug'] = $cat[0]->slug;
-      $result['post_id'] = $post->ID;
-      $result['slug'] = $post->post_name;
-      $result['content'] = apply_filters( 'the_content', $post->post_content );
-      $result['thumbnail'] = get_the_post_thumbnail( $post->ID, 'thumbnail' );
-      $result['image'] = get_the_post_thumbnail( $post->ID, 'full', array( 'class' => 'attachment' ) );
-      $result['title'] = $post->post_title;
-      $result['date'] = $post->post_date;
-      $name = get_userdata( $post->post_author );
-      $result['lastname'] = $name->user_lastname;
-      $result['firstname'] = $name->user_firstname;
-      $result['tags'] = wp_get_post_tags( $post->ID );
+      $result = $this->get_post_data( $post );
       $this->response = array( $result );
 
     // Get a group of posts.
@@ -43,36 +49,45 @@ class BackboneResponse {
       $posts = get_posts( $options );
       $results = array();
       foreach( $posts as $post ) {
-        $result = array();
-        $cat = get_the_category( $post->ID );
-        //echo var_dump( $cat );
-        $result['category'] = $cat[0]->name;
-        $result['category_id'] = $cat[0]->term_id;
-        $result['category_slug'] = $cat[0]->slug;
-        $result['post_id'] = $post->ID;
-        $result['slug'] = $post->post_name;
-        $result['content'] = apply_filters( 'the_content', $post->post_content );
-        $result['thumbnail'] = get_the_post_thumbnail( $post->ID, 'thumbnail' );
-        $result['image'] = get_the_post_thumbnail( $post->ID, 'full', array( 'class' => 'attachment' ) );
-        $result['title'] = $post->post_title;
-        $result['date'] = $post->post_date;
-        $name = get_userdata( $post->post_author );
-        $result['lastname'] = $name->user_lastname;
-        $result['firstname'] = $name->user_firstname;
-        $result['tags'] = wp_get_post_tags( $post->ID );
+        $result = $this->get_post_data( $post );
         array_push( $results, $result );
       }
       $this->response = $results;
     
+    } elseif( $this->type == 'category' ) {
+      $options = array(
+        'numberposts' => $this->number,
+        'offset' => $this->offset,
+        'exclude' => $this->exclude,
+        'category' => $this->id
+      );
+      $posts = get_posts( $options );
+      $results = array();
+      foreach( $posts as $post ) {
+        $result = $this->get_post_data( $post );
+        array_push( $results, $result );
+      }
+      $this->response = $results;
+
     // Get a list of categories.
     } elseif( $this->type == 'categories' ) {
-      $raw_cats = get_categories();
-      $cats = array();
-      foreach( $raw_cats as $k => $v ) {
-        array_push( $cats, $v );
-      }
+      $cats = get_categories(
+        array(
+          'orderby' => 'count',
+          'order' => 'DESC'
+        )
+      );
       $this->response = $cats;
-      
+
+    // Get a list of dates.
+    } elseif( $this->type == 'dates' ) {
+      $options = array(
+        'echo' => false,
+        'format' => ''
+      );
+      $dates = wp_get_archives( $options );
+      $this->response = $dates;
+
     // If the request sucks. 
     } else {
       $this->response = null;
